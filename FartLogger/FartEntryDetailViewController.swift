5845524f2120c12e32867657873f8100e6c2ccd5
James Lane <james.lane@tackmobile.com>
2020-06-18 11:07:02 -0600

    Adjust message again

--- FartLogger/FartEntryDetailViewController.swift
+++ FartLogger/FartEntryDetailViewController.swift
@@ -74,7 +74,7 @@ class FartEntryDetailViewController : UIViewController {
         formatter.timeStyle = .medium
         
         
-        let message = "Hey Guess What? Jimbo's FartLogger detected I farted at " + formatter.string(from: entry.timestamp) + "\nWhen was your last rip?"
+        let message = "Hey Guess What? I farted at " + formatter.string(from: entry.timestamp) + "\n Message courtesy of Jimbo's FartLogger. When was your last rip?"
         
         let activityVC = UIActivityViewController(activityItems: [message], applicationActivities: nil)
         self.present(activityVC, animated: true)
